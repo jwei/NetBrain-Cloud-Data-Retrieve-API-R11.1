56fa5d693be8e2a213834d117286c292afbee3e0
Xun Peng <xunpeng715@gmail.com>
2023-03-28 10:52:16 -0400

    Update get_azure_data_2steps.py

--- Microsoft Azure/Parser/get_azure_data_2steps.py
+++ Microsoft Azure/Parser/get_azure_data_2steps.py
@@ -1,21 +1,22 @@
-## `get_azure_api_data(api_server_id, url, method="GET", json_body=None)`
+## Description
+The get_azure_data_2steps function supports two steps (i.e., POST + GET) methods to get data tables of Azure resources. This function performs the following two steps to retrieve the data:
 
-This function retrieves data from the Azure API using the provided URL.
+It sends a POST request which returns a table URL in the HTTP response location header.
+It sends a subsequent GET request to get the table data from the returned URL.
+Note that it takes time for the Azure REST server to generate the table resource. The function will retry if the resource is not ready, with a maximum timeout of 30 seconds.
 
-### Parameters
+Please refer to the Microsoft API documentation for URL format and parameters. For example, please refer to https://docs.microsoft.com/en-us/rest/api/network-gateway/virtualnetworkgateways/getlearnedroutes#code-try-0
 
-- `api_server_id`: The ID of the API server.
-- `url`: The URL of the API endpoint to call.
-- `method`: The HTTP method to use for the API call (default: `"GET"`).
-- `json_body`: The JSON body to send with the API call (default: `None`).
+## Usage
+
+### Arguments
+ - `api_server_id` (str): Azure Tenant API Server Instance ID saved in the device.
+ - `url` (str): HTTP request URL of the POST method. For example, https://management.azure.com/subscriptions/073e6f45-d1ae-40fe-93af-88231d2377bd/resourceGroups/Central-RG1/providers/Microsoft.Network/virtualNetworkGateways/West-VPN-GW/getLearnedRoutes?api-version=2020-06-01
+ - `json_body` (dict): The JSON body of the POST request. For example, {'sample_field1': value1}
 
 ### Returns
-
-The data returned by the Azure API.
-
-### Usages through `RetrieveData(rtn_params)`
-
-This function retrieves data from the Azure API using the provided parameters.
+ - `dict` or `list`: The JSON output of the table result.
+ - `None`: In case of POST URL format error or GET request timeout.
 
 ### Example
 
